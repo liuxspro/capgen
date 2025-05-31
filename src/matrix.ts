4c1418423eeddd6eed992e7b360ff3ad74305691
liuxspro <liuxspro@gmail.com>
2025-05-31 13:14:24 +0000

    Feat: add matrix

--- src/matrix.ts
+++ src/matrix.ts
@@ -26,7 +26,6 @@ export function generate_tile_matrixs(
     throw new Error("Zoom levels must be integers");
   }
 
-  // 合并参数处理
   const tileSize = config.tileSize || 256;
   let baseScale = config.baseScale ?? 559_082_264.028_7178;
 
@@ -157,6 +156,30 @@ export class CRS84TileMatrixSet extends TileMatrixSet {
   }
 }
 
+export class CRS84LessTileMatrixSet extends CRS84TileMatrixSet {
+  constructor(
+    title: string,
+    id: string,
+    supported_crs: string,
+    wellknown_scale_set: string,
+    min_zoom: number,
+    max_zoom: number,
+  ) {
+    super(title, id, supported_crs, wellknown_scale_set, min_zoom, max_zoom);
+    this.tile_matrixs = this.generateMatrixs(min_zoom, max_zoom);
+  }
+  protected override generateMatrixs(min: number, max: number): TileMatrix[] {
+    const matrix = generate_tile_matrixs(min, max, { "type": "4326" });
+    const matrix_less = matrix.map((t) => {
+      const n = t.identifier;
+      const less = parseInt(n) - 1;
+      t.identifier = String(less);
+      return t;
+    });
+    return matrix_less;
+  }
+}
+
 // 常用瓦片矩阵集
 // WebMercator (256px)
 // https://docs.ogc.org/is/17-083r2/17-083r2.html#73
@@ -189,12 +212,35 @@ export const world_crs84_quad: CRS84TileMatrixSet = new CRS84TileMatrixSet(
   1,
   18,
 );
+
+// 比正常的 4326 瓦片层级少 1
+export const world_crs84_quad_less: CRS84LessTileMatrixSet =
+  new CRS84LessTileMatrixSet(
+    "CRS84 for the World",
+    "WorldCRS84QuadLess",
+    "EPSG:4326",
+    "http://www.opengis.net/def/wkss/OGC/1.0/GoogleCRS84Quad",
+    1,
+    18,
+  );
+
 // 与 EPSG:4326 基本一样
 export const cgcs2000_quad: CRS84TileMatrixSet = new CRS84TileMatrixSet(
   "CRS84 for the World",
-  "WorldCRS84Quad",
+  "CGCS2000Quad",
   "EPSG:4490",
   "http://www.opengis.net/def/wkss/OGC/1.0/GoogleCRS84Quad",
   1,
+  18,
+);
+
+// EPSG:3395
+// See https://docs.ogc.org/is/17-083r4/17-083r4.html#toc51
+export const world_mercator_quad: TileMatrixSet = new TileMatrixSet(
+  "CRS84 for the World",
+  "WorldMercatorWGS84Quad",
+  "EPSG:3395",
+  "http://www.opengis.net/def/wkss/OGC/1.0/WorldMercatorWGS84",
+  0,
   18,
 );
